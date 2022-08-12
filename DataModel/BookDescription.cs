09e231f0c0b047e532077df8b54e172f7435fe5c
Aleksejs Semjonovs <yelowcat@gmail.com>
2022-08-12 19:06:35 +0300

    Add project files.

---
+++ DataModel/BookDescription.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace DataModel
+{
+    public enum Genre
+    {
+        SciFi, Science, Classic
+    }
+
+    public class BookDescription: DataElement
+    {
+        private string? title;
+
+        [Required]
+        public string Title
+        {
+            get => title ?? "";
+            set
+            {
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string? description;
+
+        public string? Description
+        {
+            get => description;
+
+            set
+            {
+                description = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string? author;
+
+        [Required]
+        public string? Author
+        {
+            get => author;
+
+            set
+            {
+                author = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private Genre? genre;
+        public Genre? Genre
+        {
+            get => genre;
+
+            set
+            {
+                genre = value;
+                OnPropertyChanged();
+            }
+        }
+
+        
+
+
+    }
+}
---
+++ ViewModel/View.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+
+using DataModel;
+
+namespace ViewModel
+{
+    public class View : DataElement
+    {
+
+        public static RoutedCommand Add { get; set; }
+
+        public ObservableCollection<BookDescription> BookDescriptions { get; set; }
+    }
+}
