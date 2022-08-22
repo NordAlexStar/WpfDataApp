328acfe74d96fba31fa1c0c9dfe104be819bf7d8
Aleksejs Semjonovs <yelowcat@gmail.com>
2022-08-22 17:10:05 +0300

    EF

---
+++ DataModel/DB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    public class DB: DbContext
+    {
+        public DbSet<BookDescription> BookDescriptions { get; set; }  
+        
+        public DB(): base("Data Source=(LocalDb)\\LocalDb;Initial Catalog=Books;Integrated Security=SSPI")
+        { }
+    }
+}
